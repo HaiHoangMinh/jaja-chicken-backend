13c1633a8c53a3f6927788878781fbf577a9e622
HaiHoangMinh <haibg1998b@gmail.com>
2021-08-25 05:27:30 +0700

    add_my_user

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,12 +13,12 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            ['name' => 'Admin', 'username' => 'admin', 'password' => '123456789',
-            'id' => '1998','feature_image_path' => 'https://thumbs.dreamstime.com/z/gentleman-avatar-profile-icon-image-default-user-hairstyle-vector-illustration-182197665.jpg'], 
+            ['name' => 'Hoang Minh Hai', 'username' => 'hai61pm2', 'password' => md5('123456789'),
+            'id' => '2021','feature_image_path' => 'https://thumbs.dreamstime.com/z/gentleman-avatar-profile-icon-image-default-user-hairstyle-vector-illustration-182197665.jpg'], 
             
         ]);
         DB::table('role_user')->insert([
-            ['user_id' => '1998', 'role_id' => '1',], 
+            ['user_id' => '2021', 'role_id' => '1',], 
             
         ]);
     }
