144999d8b19f61630f7e7235f99cc28ba8a3bc62
HaiHoangMinh <haibg1998b@gmail.com>
2021-08-26 18:36:19 +0700

    add_seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -12,41 +12,7 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        DB::table('categories')->insert([
-            ['id'=>1,
-            'name'=>'Gà Jaja',
-            'parent_id'=>0,
-            'slug'=>'ga-jaja',], 
+        
             
-            [
-                'id'=>11,
-                'name'=>'Gà cay',
-                'parent_id'=>1,
-                'slug'=>'ga-cay',
-                'created_at'=>'2021-06-30 01:53:54',
-                'updated_at'=>'2021-06-30 01:53:54',
-                'deleted_at'=>NULL
-            ],
-            [
-                'id'=>12,
-                'name'=>'Gà truyền thống',
-                'parent_id'=>1,
-                'slug'=>'ga-truyen-thong',
-                'created_at'=>'2021-06-30 03:19:33',
-                'updated_at'=>'2021-06-30 03:19:33',
-                'deleted_at'=>NULL
-            ],
-            [
-                'id'=>13,
-                'name'=>'Gà cay ngọt',
-                'parent_id'=>20,
-                'slug'=>'ga-cay-ngot',
-                'created_at'=>'2021-06-30 03:19:48',
-                'updated_at'=>'2021-06-30 03:20:01',
-                'deleted_at'=>NULL
-                ]
-                
-            
-        ]);
     }
 }
